f6266705dba5867f6e4e51abbeed834adef50403
Anton-Dmitriichuk <dmtant26@gmail.com>
2024-03-22 00:52:42 +0300

    fewfewfe

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,34 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    "vercel-deploy": {
+      enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_oO8BzIkjX71LI3TTMuBkFv5SrzGl/pkva8FJDQ8",
+        apiToken: "pWxhtRJIsf3fy3lFBMQH6HOj",
+        roles: ["strapi-super-admin"],
+      },
+    },
+
+    "firebase-auth": {
+      enabled: true,
+      config:{ FIREBASE_JSON_ENCRYPTION_KEY:"BpNZufhGpWW2uaYacf8tl3FtGtu1npm" }
+  },
+
+
+  upload: {
+    config: {
+      provider: "strapi-provider-firebase-storage",
+      providerOptions: {
+        serviceAccount: require("C:/proj/ijora-strapi-firebase-adminsdk-d2aex-26675e35c4.json"),
+        // Custom bucket name
+        bucket: env(
+          "STORAGE_BUCKET_URL",
+          "ijora-strapi.appspot.com"
+        ),
+        sortInStorage: true, // true | false
+        debug: false, // true | false
+      },
+    },
+  },
+
+  });
